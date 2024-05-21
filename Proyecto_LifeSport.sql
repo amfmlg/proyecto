2d4536b829d11caff282543818d60bbf15219063
feernandobaena <148863645+feernandobaena@users.noreply.github.com>
2024-05-21 10:14:38 +0200

    Update Proyecto_LifeSport.sql

--- Proyecto_LifeSport.sql
+++ Proyecto_LifeSport.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: mysql:3306
--- Tiempo de generación: 23-04-2024 a las 08:33:59
--- Versión del servidor: 8.3.0
--- Versión de PHP: 8.2.8
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-04-2024 a las 09:24:37
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -94,6 +94,58 @@ CREATE TABLE `Rutina` (
   `Descanso` time NOT NULL,
   `Musculo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='TABLA DE RUTINAS';
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sportlife`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_usuarios`
+--
+
+CREATE TABLE `registro_usuarios` (
+  `Id` int(11) NOT NULL,
+  `Nombre_Usuario` varchar(30) NOT NULL,
+  `Contraseña` varchar(50) NOT NULL,
+  `Altura` int(250) NOT NULL,
+  `Peso` int(160) NOT NULL,
+  `Edad` int(130) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='TABLA DE REGISTRO DE USUARIOS';
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registro_usuarios`
+--
+ALTER TABLE `registro_usuarios`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `registro_usuarios`
+--
+ALTER TABLE `registro_usuarios`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 
 --
 -- Índices para tablas volcadas
