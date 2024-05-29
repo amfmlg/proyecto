290d2119ee8ee28f3f3f27b62adbc700e608ca0a
AMF <148863482+amfmlg@users.noreply.github.com>
2024-05-29 13:14:09 +0200

    Add files via upload

---
+++ SPORTLIFE 6.0 BEDROCK/src/main/java/com/proyectodam/biblioteca/dao/GestorUsuarios.java
@@ -0,0 +1,44 @@
+package com.proyectodam.biblioteca.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.proyectodam.biblioteca.dto.RegistroUsuario;
+
+public class GestorUsuarios {
+
+    private RegistroUsuario usuarioActual;
+
+    // Método para autenticar un usuario
+    public boolean autenticarUsuario(String nombre, String contraseña) {
+        try (Connection connection = Conexion.getConnection()) {
+            String query = "SELECT * FROM Usuario WHERE nombre = ? AND contraseña = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, nombre);
+            statement.setString(2, contraseña);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String correoElectronico = resultSet.getString("correoElectronico");
+                String fechaNacimiento = resultSet.getString("fechaDeNacimiento");
+                float peso = resultSet.getFloat("peso");
+                float altura = resultSet.getFloat("altura");
+                String frecuenciaEjercicio = resultSet.getString("frecuenciaDeEjercicio");
+                usuarioActual = new RegistroUsuario(id, nombre, contraseña, correoElectronico, fechaNacimiento, String.valueOf(peso), String.valueOf(altura), frecuenciaEjercicio);
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Método para obtener el usuario actual
+    public RegistroUsuario obtenerUsuarioActual() {
+        return usuarioActual;
+    }
+
+}
